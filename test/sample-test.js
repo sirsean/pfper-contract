51ce64392e270f80bd1d411172c5fee46f19f58e
sirsean <sirsean@gmail.com>
2022-06-26 13:50:50 -0500

    initial project

---
+++ test/sample-test.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    await greeter.setGreeting("Hola, mundo!").then(tx => tx.wait());
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
